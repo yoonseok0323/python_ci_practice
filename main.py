1daf42f01668ada38f5325a0681f670476450fbf
yoonseok0323 <yoonrock02@gmail.com>
2022-08-29 09:43:53 +0900

    a

---
+++ main.py
@@ -0,0 +1,13 @@
+def helloworld(a):
+    msg = f"Hello World! {a}!"
+    print(msg)  # hmm
+    return msg
+
+def helloworld(a):
+    msg = f"Hello World! {a}!"
+    print(msg)  # hmm
+    return msg
+
+
+if __name__ == "__main__":  # pragma: no cover
+    helloworld("Chris")
